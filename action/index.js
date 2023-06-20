f5acccad2b4548f969162a9264fa5ac015342844
Pana T <pana@code-engine.net>
2023-06-20 10:10:08 +0200

    try to remove path from element output

--- action/index.js
+++ action/index.js
@@ -3,14 +3,14 @@ const core = require('@actions/core');
 function run() {
   try {
     const changedFilesString = core.getInput('all_changed_files');  // Get changed files from the changed-files action
-    const changedFiles = changedFilesString.split(', ');  // Split into an array of file paths
+    const changedFiles = changedFilesString.split(' ');  // Split into an array of file paths
     const pathPrefix = core.getInput('path_prefix');
     const directories = [];
 
     for (const file of changedFiles) {
       const parts = file.split('/');
-      if (parts.length > 2) {
-        const directory = parts[2]; // Get the directory name directly
+      if (parts.length > 3) {
+        const directory = parts[3]; // Get the directory name (assuming the structure is aws/app/stacks/directory/file)
         if (directory) {
           const fullPath = pathPrefix ? `${pathPrefix}/${directory}` : directory; // Apply path prefix if provided
           directories.push(fullPath);
@@ -18,7 +18,7 @@ function run() {
       }
     }
 
-    const matrix = { element: directories.map(directory => directory.replace(`${pathPrefix}/`, '')) }; // Remove the path prefix from the matrix elements
+    const matrix = { element: directories };
 
     core.setOutput('matrix', JSON.stringify(matrix));
   } catch (error) {
