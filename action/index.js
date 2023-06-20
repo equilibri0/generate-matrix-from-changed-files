ec7477f44a4a7cc839844bf8c69f8ecabbc8599e
Pana T <pana@code-engine.net>
2023-06-20 10:34:15 +0200

    fixes for element seperation

--- action/index.js
+++ action/index.js
@@ -2,15 +2,23 @@ const core = require('@actions/core');
 
 function run() {
   try {
-    const changedFilesString = core.getInput('all_changed_files');  // Get changed files from the changed-files action
-    const changedFiles = changedFilesString.split(' ');  // Split into an array of file paths
+    const changedFilesString = core.getInput('all_changed_files');
+    const changedFiles = changedFilesString.split(' ');
+    console.log('Changed files:', changedFiles);
+
     const pathPrefix = core.getInput('path_prefix');
+    const prefixParts = pathPrefix.split('/').filter(Boolean); // split the prefix and filter out empty strings
+    const prefixLength = prefixParts.length;
+    console.log('Prefix length:', prefixLength); // Debug log
+
     const directories = new Set();
 
     for (const file of changedFiles) {
       const parts = file.split('/');
-      if (parts.length > 2) {
-        const directory = parts[2]; // Get the directory name directly
+      console.log('File parts:', parts);
+
+      if (parts.length > prefixLength) {
+        const directory = parts[prefixLength];
         if (directory) {
           directories.add(directory);
         }
@@ -18,6 +26,7 @@ function run() {
     }
 
     const sanitizedDirectories = Array.from(directories);
+    console.log('Directories:', sanitizedDirectories);
 
     const matrix = { element: sanitizedDirectories };
 
