ec1d68975fa5eab16f5c0912f894a83a131b6be0
Pana T <pana@code-engine.net>
2023-06-20 10:04:31 +0200

    try to remove path from element output

--- action/index.js
+++ action/index.js
@@ -1,30 +1,29 @@
 const core = require('@actions/core');
 
-try {
-  const changedFilesString = core.getInput('all_changed_files');
-  const pathPrefix = core.getInput('path_prefix');
-  const directories = [];
-
-  if (changedFilesString) {
-    const changedFiles = changedFilesString.split(' ');
+function run() {
+  try {
+    const changedFilesString = core.getInput('all_changed_files');  // Get changed files from the changed-files action
+    const changedFiles = changedFilesString.split(', ');  // Split into an array of file paths
+    const pathPrefix = core.getInput('path_prefix');
+    const directories = [];
 
     for (const file of changedFiles) {
       const parts = file.split('/');
-      if (parts.length > 3) {
-        const directory = parts[3];
+      if (parts.length > 2) {
+        const directory = parts[2]; // Get the directory name directly
         if (directory) {
-          const fullPath = pathPrefix ? `${pathPrefix}/${directory}` : directory;
-          if (!directories.includes(fullPath)) {
-            directories.push(fullPath);
-          }
+          const fullPath = pathPrefix ? `${pathPrefix}/${directory}` : directory; // Apply path prefix if provided
+          directories.push(fullPath);
         }
       }
     }
+
+    const matrix = { element: directories.map(directory => directory.replace(`${pathPrefix}/`, '')) }; // Remove the path prefix from the matrix elements
+
+    core.setOutput('matrix', JSON.stringify(matrix));
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  const matrix = { element: directories };
-
-  core.setOutput('matrix', JSON.stringify(matrix));
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
