26f33a461c97121b9e57d77de867429f9586649d
Pana T <pana@code-engine.net>
2023-06-20 09:10:31 +0200

    added debug statement

--- action/index.js
+++ action/index.js
@@ -1,29 +1,28 @@
 const core = require('@actions/core');
 
-function run() {
-  try {
-    const changedFilesString = core.getInput('all_changed_files');  // Get changed files from the changed-files action
-    const changedFiles = changedFilesString.split(', ');  // Split into an array of file paths
-    const pathPrefix = core.getInput('path_prefix');
-    const directories = [];
+try {
+  const changedFilesString = core.getInput('all_changed_files');
+  const changedFiles = changedFilesString.split(', ');
+  const pathPrefix = core.getInput('path_prefix');
+  const directories = [];
 
-    for (const file of changedFiles) {
-      const parts = file.split('/');
-      if (parts.length > 2) {
-        const directory = parts.slice(2, -1).join('/'); // Get the directory name by removing the last part of the file path
-        if (directory) {
-          const fullPath = pathPrefix ? `${pathPrefix}/${directory}` : directory; // Apply path prefix if provided
-          directories.push(fullPath);
-        }
+  for (const file of changedFiles) {
+    const parts = file.split('/');
+    if (parts.length > 2) {
+      const directory = parts[2];
+      if (directory) {
+        const fullPath = pathPrefix ? `${pathPrefix}/${directory}` : directory;
+        directories.push(fullPath);
       }
     }
-
-    const matrix = { element: directories };
-
-    core.setOutput('matrix', JSON.stringify(matrix));
-  } catch (error) {
-    core.setFailed(error.message);
   }
-}
 
-run();
+  const matrix = { element: directories };
+
+  core.setOutput('matrix', JSON.stringify(matrix));
+  
+  console.log('Matrix Output:', JSON.stringify(matrix)); // Debug statement
+
+} catch (error) {
+  core.setFailed(error.message);
+}
