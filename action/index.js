2827d31101ffdc0b01d194e6b8e77784e97572fb
Pana T <pana@code-engine.net>
2023-06-20 07:38:10 +0200

    try to fix output array

--- action/index.js
+++ action/index.js
@@ -5,17 +5,20 @@ function run() {
     const changedFilesString = core.getInput('all_changed_files');  // Get changed files from the changed-files action
     const changedFiles = changedFilesString.split(', ');  // Split into an array of file paths
     const pathPrefix = core.getInput('path_prefix');
-    const directories = {};
+    const directories = [];
 
     for (const file of changedFiles) {
-      const directory = file.replace(pathPrefix, '').split('/')[0];
-      if (directory in directories) {
-        directories[directory] = true;
+      const parts = file.split('/');
+      if (parts.length > 2) {
+        const directory = parts[2]; // Get the directory name directly
+        if (directory) {
+          const fullPath = pathPrefix ? `${pathPrefix}/${directory}` : directory; // Apply path prefix if provided
+          directories.push(fullPath);
+        }
       }
     }
 
-    const elements = Object.keys(directories);
-    const matrix = { element: elements };
+    const matrix = { element: directories };
 
     core.setOutput('matrix', JSON.stringify(matrix));
   } catch (error) {
